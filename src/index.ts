0aeb665cd016f78bdea93effa0eadeffe1eeb0a0
denis-itskovich <denis.itskovich@dell.com>
2018-09-04 14:13:17 +0300

    rxrpc-ng 0.3.6 adding rxrpc-types to index

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from './lib/rxrpc-transport';
 export * from './lib/rxrpc-client.module';
 export * from './lib/rxrpc-websocket-transport';
 export * from './lib/rxrpc-operators';
+export * from './lib/rxrpc-types';
