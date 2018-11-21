8f8f296811ac1989ba99f7aaa18355368357b370
denis-itskovich <denis.itskovich@dell.com>
2018-11-21 10:44:17 +0200

    Upgrading ng-rxrpc to 0.3.12

--- src/lib/rxrpc-websocket-transport.ts
+++ src/lib/rxrpc-websocket-transport.ts
@@ -1,20 +1,25 @@
 import {RxRpcConnection, RxRpcTransport} from './rxrpc-transport';
-import {webSocket} from 'rxjs/webSocket'
+import {webSocket, WebSocketSubjectConfig} from 'rxjs/webSocket'
 import {Observable} from 'rxjs';
 
-export class RxRpcWebSocketTransport extends RxRpcTransport {
-    constructor(private readonly url: string) {
-        super();
+export class RxRpcWebSocketTransport implements RxRpcTransport {
+    private readonly config: WebSocketSubjectConfig<any>;
+
+    constructor(urlOrConfig: string|WebSocketSubjectConfig<any>) {
+        this.config = typeof urlOrConfig === 'string'
+            ? {url: urlOrConfig}
+            : <WebSocketSubjectConfig<any>>urlOrConfig;
     }
 
     connect(): Observable<RxRpcConnection> {
         return Observable.create(observer => {
-            const ws = webSocket(this.url);
+            const ws = webSocket(this.config);
             observer.next({
                 messages: ws,
                 send: msg => ws.next(msg),
                 close: () => ws.complete()
-            })
+            });
+            observer.complete();
         });
     }
 }
