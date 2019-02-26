002071f84846683728b808b40cac0c94f40d170f
denis-itskovich <denis.itskovich@dell.com>
2019-02-26 13:54:41 +0200

    [rxrpc-ng] Fixing deployment

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 npm version patch -m "Updating version to %s" --git-tag-version
+git push && git push --tags
 echo //registry.npmjs.org/:_authToken=$NPM_API_KEY > ~/.npmrc
 npm publish
