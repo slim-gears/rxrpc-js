d54e589e1dfecc739c7e5e53eb1d4b0fc841eeb1
denis-itskovich <denis.itskovich@dell.com>
2019-07-01 12:29:55 +0300

    [rxrpc-js] Fixing yarn deployment

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo //registry.npmjs.org/:_authToken=$NPM_API_KEY > ~/.npmrc
-git stash && yarn version $1 && yarn publish
+echo //registry.npmjs.org/:_authToken=${NPM_API_KEY} > ~/.npmrc
+git stash && yarn --new-version $1 && yarn publish
