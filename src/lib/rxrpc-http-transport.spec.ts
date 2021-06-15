980d3e2f957c43f60fdfc9e204f7d72ceaae1f7f
Koaz <Uri.Koaz@emc.com>
2021-06-15 12:36:03 +0300

    add intercptors in http options
    Signed-off-by: Koaz <Uri.Koaz@emc.com>

--- src/lib/rxrpc-http-transport.spec.ts
+++ src/lib/rxrpc-http-transport.spec.ts
@@ -1,5 +1,12 @@
-import {RxRpcHttpConnection, RxRpcHttpTransport} from './rxrpc-http-transport';
+import {
+    RxRpcHttpConnection,
+    RxRpcHttpTransport,
+    RxRpcHttpTransportInterceptor,
+    RxRpcHttpTransportRequestConfig
+} from './rxrpc-http-transport';
 import axios from 'axios';
+import {Observable, of} from "rxjs";
+import {HttpAttributes} from "./rxrpc-http-attributes";
 
 jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>
@@ -14,8 +21,28 @@ describe('RxRpc Http Transport test suite', function () {
     let incomingMessages: any[];
     let resp: {}
 
+    let interceptors: RxRpcHttpTransportInterceptor[] = [];
+    let headerKey1 = "hk1";
+    let headerKey2 = "hk2"
+    let headerValue1 = "value1";
+    let headerValue2 = "value2";
+
     beforeEach(() => {
-        transport = new RxRpcHttpTransport("https://funnyName/");
+        interceptors.push(new class implements RxRpcHttpTransportInterceptor {
+            intercept(requestConfig: RxRpcHttpTransportRequestConfig): Observable<RxRpcHttpTransportRequestConfig> {
+                requestConfig.headers[headerKey1] = headerValue1;
+                return of(requestConfig);
+            }
+        })
+        interceptors.push(new class implements RxRpcHttpTransportInterceptor {
+            intercept(requestConfig: RxRpcHttpTransportRequestConfig): Observable<RxRpcHttpTransportRequestConfig> {
+                requestConfig.headers[headerKey2] = headerValue2;
+                return of(requestConfig);
+            }
+        })
+
+        transport = new RxRpcHttpTransport("https://funnyName/", {interceptors: interceptors});
+
         clientId = "12345678";
         incomingMessages = [];
         resp = {
@@ -25,7 +52,6 @@ describe('RxRpc Http Transport test suite', function () {
 
     it('Connect', async () => {
         mockedAxios.post.mockImplementation(() => Promise.resolve(resp));
-
         transport.connect().subscribe(connection => incomingMessages.push(connection['clientId']))
         await delay(1000);
         expect(incomingMessages[0]).toEqual(clientId)
@@ -35,12 +61,15 @@ describe('RxRpc Http Transport test suite', function () {
         const data1 = "{\"invocationId\":1,\"result\":{\"type\":\"Data\",\"data\":\"Hello, Angular #0\",\"error\":null}}"
         const data2 = "{\"invocationId\":1,\"result\":{\"type\":\"Data\",\"data\":\"Hello, Angular #1\",\"error\":null}}"
         resp['data'] = JSON.parse(`[${data1},\n${data2}]`)
-        mockedAxios.post.mockImplementation(() => Promise.resolve(resp));
+        mockedAxios.post.mockImplementationOnce(() => Promise.resolve(resp));
 
         transport.connect().subscribe(connection => connection.poll().subscribe(msg => {
+            addHeadersVerifier();
             incomingMessages.push(msg);
         }))
+
         await delay(1000);
+
         expect(incomingMessages.length).toEqual(2);
         expect(incomingMessages[0]).toEqual(JSON.parse(data1));
         expect(incomingMessages[1]).toEqual(JSON.parse(data2));
@@ -49,11 +78,14 @@ describe('RxRpc Http Transport test suite', function () {
     it('Poll with data as JSON', async () => {
         const data = "{\"invocationId\":1,\"result\":{\"type\":\"Data\",\"data\":\"Hello, Angular #0\",\"error\":null}}"
         resp['data'] = JSON.parse(`[${data}]`)
-        mockedAxios.post.mockImplementation(() => Promise.resolve(resp));
+        mockedAxios.post.mockImplementationOnce(() => Promise.resolve(resp));
+        transport.connect().subscribe(connection => {
+            addHeadersVerifier();
+            connection.poll().subscribe(msg => {
+                incomingMessages.push(msg);
+            })
+        })
 
-        transport.connect().subscribe(connection => connection.poll().subscribe(msg => {
-            incomingMessages.push(msg);
-        }))
         await delay(1000);
         expect(incomingMessages.length).toEqual(1);
         expect(incomingMessages[0]).toEqual(JSON.parse(data));
@@ -78,4 +110,15 @@ describe('RxRpc Http Transport test suite', function () {
         connection.error(null)
         expect(connection['pollingSubscription'].closed).toEqual(true)
     })
+
+    function addHeadersVerifier() {
+        mockedAxios.post.mockImplementation((url, msg, options) => {
+            let expectedHeaders = {};
+            expectedHeaders[HttpAttributes.ClientIdAttribute] = clientId;
+            expectedHeaders[headerKey1] = headerValue1;
+            expectedHeaders[headerKey2] = headerValue2;
+            expect(options.headers).toEqual(expectedHeaders);
+            return Promise.resolve(resp);
+        });
+    }
 })
--- src/lib/rxrpc-http-transport.ts
+++ src/lib/rxrpc-http-transport.ts
@@ -1,17 +1,27 @@
 import {RxRpcConnection, RxRpcTransport} from './rxrpc-transport';
 import {interval, Observable, of, Subject, Subscription} from 'rxjs';
-import {mergeMap, map, retry, filter, tap} from "rxjs/operators";
+import {map, mergeMap, retry, tap} from "rxjs/operators";
 import {HttpAttributes} from "./rxrpc-http-attributes";
-import axios, {AxiosResponse} from 'axios';
+import axios from 'axios';
 import {fromPromise} from "rxjs/internal-compatibility";
 import {fromArray} from "rxjs/internal/observable/fromArray";
 
 export interface RxRpcHttpTransportOptions {
     pollingPeriodMillis?: number
     pollingRetryCount?: number
+    interceptors?: RxRpcHttpTransportInterceptor[]
+}
+
+export interface RxRpcHttpTransportRequestConfig {
+    headers: {[key: string]: string}
+}
+
+export interface RxRpcHttpTransportInterceptor {
+    intercept(requestConfig: RxRpcHttpTransportRequestConfig) : Observable<RxRpcHttpTransportRequestConfig>;
 }
 
 export class RxRpcHttpConnection implements RxRpcConnection {
+    readonly interceptors: RxRpcHttpTransportInterceptor[] = [];
     readonly messages: Observable<any>;
     private pollingSubscription: Subscription;
     private readonly incoming = new Subject<any>();
@@ -19,6 +29,8 @@ export class RxRpcHttpConnection implements RxRpcConnection {
     constructor(private readonly uri: string,
                 private readonly clientId: string,
                 options: RxRpcHttpTransportOptions) {
+
+        this.interceptors = options.interceptors || [];
         this.messages = this.incoming;
         this.pollingSubscription = interval(options.pollingPeriodMillis)
             .pipe(
@@ -48,18 +60,25 @@ export class RxRpcHttpConnection implements RxRpcConnection {
 
     post(path: string, msg?: any): Observable<any> {
         const headers = {};
+
         headers[HttpAttributes.ClientIdAttribute] = this.clientId;
-        return fromPromise(axios.post<string>(`${this.uri}/${path}`, msg, {headers: headers}))
-            .pipe(
-                map(resp => resp.data),
-                mergeMap(data => {
-                    return fromArray(data);
-                }),
-                tap(
-                obj => this.incoming.next(obj),
-                err => this.incoming.error(err)
-                )
-            );
+        let config: Observable<RxRpcHttpTransportRequestConfig> = of({headers: headers});
+
+        // flatten all interceptor observable
+        this.interceptors.forEach(interceptor => config = config.pipe(mergeMap(cfg => interceptor.intercept(cfg))));
+
+        return config.pipe(mergeMap(cfg =>
+            fromPromise(axios.post<string>(`${this.uri}/${path}`, msg, {headers: cfg.headers}))
+                .pipe(
+                    map(resp => resp.data),
+                    mergeMap(data => {
+                        return fromArray(data);
+                    }),
+                    tap(
+                        obj => this.incoming.next(obj),
+                        err => this.incoming.error(err)
+                    )
+            )));
     }
 }
 
@@ -67,7 +86,8 @@ export class RxRpcHttpTransport implements RxRpcTransport {
     private readonly options: RxRpcHttpTransportOptions;
     private static readonly defaultOptions: RxRpcHttpTransportOptions = {
         pollingPeriodMillis: HttpAttributes.DefaultClientPollingPeriodMillis,
-        pollingRetryCount: HttpAttributes.DefaultClientPollingRetryCount
+        pollingRetryCount: HttpAttributes.DefaultClientPollingRetryCount,
+        interceptors: []
     }
 
     constructor(private readonly uri: string, options?: RxRpcHttpTransportOptions) {
